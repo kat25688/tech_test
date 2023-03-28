6ac83f0c99219a0fede19d34dc123a02755a73c7
Kat Mason <kat25688@gmail.com>
2023-03-29 00:44:27 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -17,13 +17,19 @@ function App() {
         const jsonData = await response.json();
         console.log(jsonData); // result results as an object
         let tableData = "";
+        // displaying that able to return 1 set of results
+        console.log(`first id result: ${jsonData.items[0].id}`);
+        console.log(`first login result: ${jsonData.items[0].owner.login}`);
+        console.log(
+          `first star count result: ${jsonData.items[0].stargazers_count}`
+        );
 
         //for each result return id, owner > login, star count
         jsonData.forEach((result) => {
           tableData += `<tr>
-            <td>${result.items[0].id}</td>
-            <td>${result.items[0].owner.login}</td>
-            <td>${result.items[0].stargazers_count}</td>
+            <td>${result.items.id}</td>
+            <td>${result.items.owner.login}</td>
+            <td>${result.items.stargazers_count}</td>
             </tr>`;
         });
 
@@ -79,3 +85,36 @@ function App() {
 }
 
 export default App;
+
+//====checklist=====
+// - search box
+
+// Output as a table
+// - repo name (id)
+// - author (owner > login)
+// - number of stars (stargazers_count)
+
+// Sort by
+// - number of stars
+
+// Notes
+// - Sets of 25
+// - next/previous (only show previous if valid)
+
+//=====tasks complete / issues =========
+// created search box and button - STATIC doesn't search
+// created table in react - DONE
+
+// return data from API - console all data, having issue with displaying specific
+//data due to some data at child level which is stopping my forEach from running
+//ERROR: TypeError: jsonData.forEach is not a function
+
+// return 25 results at a time - API total of 30 unable to add function due to no results returned
+
+// added previous and next buttons - both STATIC, would ideally like the previous button to not
+//show or be disabled when the first 25 results are returned
+
+// sort order number of stars - didn't attempt
+
+// issue with trying to commit to github - terminal shows current branch is behind, git stash, git stash clear but still having issues
+//error: failed to push some refs to 'https://github.com/kat25688/tech_test'
